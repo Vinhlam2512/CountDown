3062024747d7307bfabbc21bae61704fc176373f
Vinhlam2512 <lamvinh2431@gmail.com>
2021-01-10 00:28:00 +0700

    countdown

---
+++ countdown.js
@@ -0,0 +1,31 @@
+
+const newYear = '12 February 2021';
+
+const countdownDays = document.getElementById("days");
+const countdownHours = document.getElementById("hours");
+const countdownMinutes = document.getElementById("minutes");
+const countdownSeconds = document.getElementById("seconds");
+
+function countDown () {
+    const newYearDates = new Date(newYear);
+    const currentDate = new Date();
+    
+    const totalSeconds = (newYearDates - currentDate) / 1000;
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+    const minutes = Math.floor(totalSeconds / 60) % 60;
+    const seconds = Math.floor(totalSeconds  % 60);
+
+    countdownDays.innerHTML = days;
+    countdownHours.innerHTML = format(hours);
+    countdownMinutes.innerHTML = format(minutes);
+    countdownSeconds.innerHTML = format(seconds);
+
+} format
+function format (time) {
+    return time < 10 ? (`0${time}`) : time ;
+}
+
+countDown();
+
+setInterval(countDown, 1000);
